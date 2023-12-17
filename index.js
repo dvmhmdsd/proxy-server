b4828a9326f21728b52aebd79085f51bf8f131f3
Mohamed <msaad@goatar.com>
2023-12-17 14:20:52 +0200

    Forward to atar prod server

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const httpProxy = require('http-proxy');
 
 const PORT = process.env.PORT || 8001
 
-httpProxy.createProxyServer({target:'https://api-dev.hectare.app', changeOrigin: true}).listen(PORT); 
+httpProxy.createProxyServer({target:'https://atarcloud.com', changeOrigin: true}).listen(PORT); 
