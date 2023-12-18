315ad4a69d1cbb47d36daf7c34b347f6da404f8d
Mohamed <msaad@goatar.com>
2023-12-18 10:10:34 +0200

    Try to add cors headers

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const httpProxy = require('http-proxy');
+const httpProxy = require("http-proxy");
 
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 8001;
 
-httpProxy.createProxyServer({target:'https://atarcloud.com', changeOrigin: true}).listen(PORT); 
+const proxy = httpProxy.createProxyServer({
+  target: "https://atarcloud.com",
+  changeOrigin: true,
+});
+
+proxy.on("proxyRes", function (proxyRes, req, res) {
+  proxyRes.headers["access-control-allow-origin"] = "*";
+  proxyRes.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE";
+});
+
+proxy.listen(PORT);
